481577eec97c21f8f4de893ab59b1868176d4714
pc <1124051606@qq.com>
2018-07-21 18:32:47 +0800

    ddd

---
+++ src/main/java/com/example/demo/Constants.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class Constants {
+
+    public static final String MQ_NAME = "admin";
+    
+    public static final String MQ_PASSWORD = "admin";
+    
+    public static final String MQ_BROKETURL = "tcp://192.168.0.103:61616";
+}
---
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println(1111);
+	}
+
+	@Test
+	public void contextLoads1() {
+	}
+}
